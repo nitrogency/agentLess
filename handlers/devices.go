b9274f412b8c5d84a2d1397acad2d233d49d282d
nitrogency <no-antenna@tutanota.com>
2025-08-24 13:11:05 +0300

    Revert "First iteration of Materialize CSS restyling, handler fixes"
    This reverts commit f8d116be0034c5250c92b17781746783fe1398c7.

--- handlers/devices.go
+++ handlers/devices.go
@@ -1,12 +1,8 @@
 package handlers
 
 import (
-	"log"
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 
-	"example/go-website/db"
 	"example/go-website/templates"
 )
 
@@ -14,17 +10,6 @@ import (
 func DevicesHandler(c *gin.Context) {
 	data := templates.GetPageDataFromGin(c)
 	data.Title = "Devices"
-
-	// Fetch all devices from database
-	devices, err := db.GetAllDevices()
-	if err != nil {
-		log.Printf("Error fetching devices: %v", err)
-		data.Error = "Failed to load devices"
-		data.Devices = []db.Device{} // Empty slice to prevent template errors
-	} else {
-		data.Devices = devices
-	}
-
 	templates.RenderGinTemplate(c, "devices", data)
 }
 
@@ -39,42 +24,6 @@ func AddDeviceHandler(c *gin.Context) {
 func EditDeviceHandler(c *gin.Context) {
 	data := templates.GetPageDataFromGin(c)
 	data.Title = "Edit Device"
-
-	// Get device ID from URL parameter
-	deviceIDStr := c.Param("id")
-	if deviceIDStr == "" {
-		data.Error = "Device ID is required"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	// Parse device ID
-	deviceID, err := strconv.ParseInt(deviceIDStr, 10, 64)
-	if err != nil {
-		log.Printf("Invalid device ID: %v", err)
-		data.Error = "Invalid device ID"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	// Load device from database
-	device, err := db.GetDeviceByID(deviceID)
-	if err != nil {
-		log.Printf("Error loading device ID %d: %v", deviceID, err)
-		data.Error = "Device not found"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	if device == nil {
-		data.Error = "Device not found"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	// Populate device data
-	data.Device = *device
-
 	templates.RenderGinTemplate(c, "edit-device", data)
 }
 
@@ -82,42 +31,6 @@ func EditDeviceHandler(c *gin.Context) {
 func DeleteDeviceHandler(c *gin.Context) {
 	data := templates.GetPageDataFromGin(c)
 	data.Title = "Delete Device"
-
-	// Get device ID from URL parameter
-	deviceIDStr := c.Param("id")
-	if deviceIDStr == "" {
-		data.Error = "Device ID is required"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	// Parse device ID
-	deviceID, err := strconv.ParseInt(deviceIDStr, 10, 64)
-	if err != nil {
-		log.Printf("Invalid device ID: %v", err)
-		data.Error = "Invalid device ID"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	// Load device from database
-	device, err := db.GetDeviceByID(deviceID)
-	if err != nil {
-		log.Printf("Error loading device ID %d: %v", deviceID, err)
-		data.Error = "Device not found"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	if device == nil {
-		data.Error = "Device not found"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	// Populate device data
-	data.Device = *device
-
 	templates.RenderGinTemplate(c, "delete-device-confirm", data)
 }
 
@@ -125,41 +38,5 @@ func DeleteDeviceHandler(c *gin.Context) {
 func MonitorDeviceHandler(c *gin.Context) {
 	data := templates.GetPageDataFromGin(c)
 	data.Title = "Monitor Device"
-
-	// Get device ID from URL parameter
-	deviceIDStr := c.Param("id")
-	if deviceIDStr == "" {
-		data.Error = "Device ID is required"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	// Parse device ID
-	deviceID, err := strconv.ParseInt(deviceIDStr, 10, 64)
-	if err != nil {
-		log.Printf("Invalid device ID: %v", err)
-		data.Error = "Invalid device ID"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	// Load device from database
-	device, err := db.GetDeviceByID(deviceID)
-	if err != nil {
-		log.Printf("Error loading device ID %d: %v", deviceID, err)
-		data.Error = "Device not found"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	if device == nil {
-		data.Error = "Device not found"
-		templates.RenderGinTemplate(c, "404", data)
-		return
-	}
-
-	// Populate device data
-	data.Device = *device
-
 	templates.RenderGinTemplate(c, "monitor-device", data)
 }
--- handlers/home.go
+++ handlers/home.go
@@ -2,9 +2,7 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
 
-	"example/go-website/db"
 	"example/go-website/templates"
 )
 
@@ -13,33 +11,10 @@ func HomeHandler(c *gin.Context) {
 	data := templates.GetPageDataFromGin(c)
 	data.Title = "Dashboard"
 
-	// Get actual statistics from database
-	// Device count
-	devices, err := db.GetAllDevices()
-	if err != nil {
-		log.Printf("Error getting devices: %v", err)
-		data.Data["DeviceCount"] = 0
-	} else {
-		data.Data["DeviceCount"] = len(devices)
-	}
-
-	// User count
-	users, err := db.GetAllUsers()
-	if err != nil {
-		log.Printf("Error getting users: %v", err)
-		data.Data["UserCount"] = 0
-	} else {
-		data.Data["UserCount"] = len(users)
-	}
-
-	// Log count
-	logs, err := db.GetAllAuditLogs()
-	if err != nil {
-		log.Printf("Error getting audit logs: %v", err)
-		data.Data["LogCount"] = 0
-	} else {
-		data.Data["LogCount"] = len(logs)
-	}
+	// Get device statistics (simplified - these functions may not exist)
+	data.Data["TotalDevices"] = 0
+	data.Data["OnlineDevices"] = 0
+	data.Data["RecentLogs"] = 0
 
 	templates.RenderGinTemplate(c, "home", data)
 }
